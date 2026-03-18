720b5e0bf1b861eb772fa131e695278418c0d095
Tife35 <boluwatifeoresanya9@gmail.com>
2026-03-19 00:49:43 +0100

    Create App.js
    Add App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header.jsx";
+import Quiz from "./components/Quiz.jsx";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
+}
